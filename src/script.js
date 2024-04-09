cde5eb202cadcf6a32c9466a1c8175e8c6b3b220
Marola <126696230+Maarola@users.noreply.github.com>
2024-04-09 03:19:03 -0300

    att

--- src/script.js
+++ src/script.js
@@ -6,3 +6,17 @@ window.addEventListener("scroll", function(){
 
     scrolldown.classList.toggle("end", window.scrollY > 20 );
 });
+
+// function header
+
+window.addEventListener("scroll", function(){
+    let inicio = document.querySelector(".inicio");
+    let sobre = document.querySelector(".sobre");
+    let projeto = document.querySelector(".projeto");
+    let contato = document.querySelector(".contato");
+
+    inicio.classList.toggle("novacor", window.scrollY < 960);
+    sobre.classList.toggle("novacor", window.scrollY > 960 && window.scrollY < 1850);
+    projeto.classList.toggle("novacor", window.scrollY > 1850 && window.scrollY < 2500);
+    contato.classList.toggle("novacor", window.scrollY > 2500);
+});
